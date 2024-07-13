17c0eaeb5fe638425cb8f7a58fcaa3a43d000533
Rajan Walia <rajanwalia4@gmail.com>
2024-07-13 13:11:57 +0530

    Server setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// Git 
+const express = require('express');
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/', function (request, response) {
+    console.log("Query ",request.query);
+    console.log("Params ",request.params);
+    response.send("Hello World ");
+});
+
+
+app.post('/', function (request, response) {
+    console.log("body ",request.body);
+    response.send("Hello World ");
+});
+
+app.listen(3000,function(){
+    console.log('App Listening to port 3000') 
+});
